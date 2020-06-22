21bf1f4fdfb111d479eafbd3cb6ffe53c21af9eb
Oleg <moskvichovoleg@gmail.com>
2020-06-22 22:38:20 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,77 @@
+import os
+from threading import Thread
+
+from flask import Flask, request, render_template, abort, Response
+from flask_mail import Mail, Message
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
+
+SENDER = 'moskvichovoleg@gmail.com'
+SENDER_PASS = '$Manners7'
+RECIPIENTS = ['moskvichovoleg@yahoo.com']
+
+app.config.update(dict(
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT=587,
+    MAIL_USE_TLS=True,
+    MAIL_USE_SSL=False,
+    MAIL_DEBUG=False,
+    MAIL_USERNAME='%s' % SENDER,
+    MAIL_PASSWORD='%s' % SENDER_PASS,
+    MAIL_DEFAULT_SENDER='%s' % SENDER,
+    MAIL_MAX_EMAILS=None,
+    MAIL_ASCII_ATTACHMENTS=False,
+))
+
+mail = Mail(app)
+
+
+def send_email(msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
+    abort(405)
+
+
+@app.route('/form/', methods=['POST'])
+def form():
+    if request.method == 'POST' and request.json:
+        tel = request.json.get('tel')
+        from_where = request.json.get('fromWhere')
+        to_where = request.json.get('toWhere')
+        name = request.json.get('name')
+        age = request.json.get('age')
+        diagnosis = request.json.get('diagnosis')
+        treatment = request.json.get('treatment')
+        escort = request.json.get('escort')
+        comments = request.json.get('comments')
+
+        print(name, tel)
+
+        msg = Message('Новая заявка с сайта!',
+                      sender=SENDER,
+                      recipients=RECIPIENTS)
+
+        body = 'Новая заявка с сайта! <br>' \
+               'Имя: %s<br>' \
+               'Телефон: %s<br>' % (name, tel)
+
+        msg.body = '%s' % body
+        msg.html = "<b>%s</b>" % body
+
+        Thread(target=send_email, args=(msg,)).start()
+
+        return Response(status=200)
+    else:
+        abort(405)
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
---
+++ static/js/index.js
@@ -0,0 +1,98 @@
+ymaps.ready(initMap);
+
+let myMap;
+
+function initMap() {
+    myMap = new ymaps.Map("map", {
+        center: [55.76, 37.64],
+        zoom: 10,
+        controls: ['zoomControl']
+    }, {suppressMapOpenBlock: true});
+
+    myMap.behaviors.disable('scrollZoom');
+    myMap.behaviors.disable('drag');
+    myMap.behaviors.disable('multiTouch');
+
+    let position = myMap.getGlobalPixelCenter();
+    myMap.setGlobalPixelCenter([position[0] - 400, position[1]]);
+}
+
+
+const detailsBtn = document.querySelector('.details-btn'),
+    details = document.querySelector('.details'),
+    addressBtn = document.querySelector('.form__address-btn'),
+    inputToWhere = document.querySelector('.form__input_to-where');
+
+
+detailsBtn && detailsBtn.addEventListener('click', e => {
+    e.preventDefault();
+    details.classList.toggle('details_active');
+    myMap.container.fitToViewport();
+});
+
+
+addressBtn && addressBtn.addEventListener('click', e => {
+    e.preventDefault();
+    inputToWhere.classList.toggle('form__input_to-where_active');
+    addressBtn.parentNode.removeChild(addressBtn);
+    myMap.container.fitToViewport();
+});
+
+
+const ambulances = [].slice.call(document.getElementsByClassName('form__ambulance'));
+
+
+ambulances.forEach((element, index) => {
+    element.addEventListener('click', e => {
+        e.currentTarget.classList.add('form__ambulance_active');
+        for (let ambulance of ambulances) {
+            if (ambulance !== e.currentTarget) {
+                ambulance.classList.remove('form__ambulance_active');
+            }
+        }
+    });
+});
+
+
+/// FORM
+
+const errorValidTel = 'Номер телефона не валидный',
+    successMessage = 'Ваша заявка отправлена',
+    telPattern = /^[\+|\d][\d\(\)\-\s]{7,50}$/i;
+
+const indexForm = document.querySelector('#form');
+
+
+indexForm && indexForm.addEventListener('submit', formListener);
+
+function formListener(e) {
+    e.preventDefault();
+
+    let form = e.target;
+    let tel = form.querySelector('[name="tel"]').value;
+    let fromWhere = form.querySelector('[name="fromWhere"]').value;
+    let toWhere = form.querySelector('[name="toWhere"]').value;
+    let name = form.querySelector('[name="name"]').value;
+    let age = form.querySelector('[name="age"]').value;
+    let diagnosis = form.querySelector('[name="diagnosis"]').value;
+    let treatment = form.querySelector('[name="treatment"]').value;
+    let escort = form.querySelector('[name="escort"]').value;
+    let comments = form.querySelector('[name="comments"]').value;
+
+    if (!telPattern.test(tel)) {
+        alert(errorValidTel);
+    } else {
+        let data = {tel, fromWhere, toWhere, name, age, diagnosis, treatment, escort, comments};
+        fetch('/form/', {
+            method: 'POST',
+            credentials: "include",
+            headers: {'content-type': 'application/json'},
+            body: JSON.stringify(data)
+        })
+            .then(() => {
+                alert(successMessage)
+            })
+            .catch(e => console.log(e))
+    }
+
+}
